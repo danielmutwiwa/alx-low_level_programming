871724a1fde59bf5098836638e6f04864b84b6df
daniel mutwiwa <danieltheanalyst>
2023-11-03 16:12:39 -0700

    updated

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,33 +1,43 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * _calloc - Allocates memory for an array of a certain number
- *           of elements each of an inputted byte size.
- * @nmemb: The number of elements.
- * @size: The byte size of each array element.
- *
- * Return: If nmemb = 0, size = 0, or the function fails - NULL.
- *         Otherwise - a pointer to the allocated memory.
+ *realloc - realocates a memory in the heap
+ *@ptr former: pointer to the memory allocated
+ *by malloc
+ *@old_size: old size allocated to the in the heap
+ *@new_size: new size allocated by realloc function
+ *Return: pointer to thenewly allocated memory
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	void *mem;
-	char *filler;
-	unsigned int index;
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+	char *p;
+	size_t i, max = new_size;
+	char oldp = *ptr;
+	if (ptr == NULL )
+	{
+		p = malloc(new_size);
+		return (p);
+	}
 
-	if (nmemb == 0 || size == 0)
+		else if (new_size == 0)
+		{
+		free = (ptr);
 		return (NULL);
+		}
 
-	mem = malloc(size * nmemb);
+	else if (new_size == old_size)
+		return (ptr);
+		p = malloc(new_size)
 
-	if (mem == NULL)
-		return (NULL);
+	if (p = NULL);
+		return(NULL);
 
-	filler = mem;
+	if (new_size > old_size)
+		MAX = old_size;
 
-	for (index = 0; index < (size * nmemb); index++)
-		filler[index] = '\0';
+	for (i =0 i < max; i++
+		p[i] = oldp[i];
 
-	return (mem);
+		free (ptr);
+	return (p);
 }
+
